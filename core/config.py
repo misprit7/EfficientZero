eb22403064e249833e4056de21fabea3d8dd8fc2
unknown <aileenrzhangg@gmail.com>
2024-03-30 09:14:29 -0700

    Made flags turn off EfficientZero

--- core/config.py
+++ core/config.py
@@ -385,9 +385,9 @@ class BaseConfig(object):
         self.gpu_actor = args.gpu_actor
         self.p_mcts_num = args.p_mcts_num
         self.use_root_value = args.use_root_value
-        self.do_consistency = not args.do_consistency_off
-        self.use_value_prefix = not args.use_value_prefix_off
-        self.off_correction = not args.off_correction_off
+        self.do_consistency = args.do_consistency_off
+        self.use_value_prefix = args.use_value_prefix_off
+        self.off_correction = args.off_correction_off
 
         if not self.do_consistency:
             self.consistency_coeff = 0
